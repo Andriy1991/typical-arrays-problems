8276f584482597a1c2b1135829345664d14f8919
Andriy1991 <andriy-5199029@mail.ru>
2021-03-14 22:12:13 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
+let array = [1, 2, 3, 4];
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.min(...array);
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.max(...array);
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
+  if (typeof array !== 'undefined' && array.length > 0) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  if (i == (array.length - 1)) {
+    return sum/array.length;
+    }
+  }
+}
+else {
   return 0;
+}
 }
